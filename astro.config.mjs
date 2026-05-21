c457a9204aa762cc9896f3e176d5d8620936357e
PapaGreen <jacob.wilder826@gmail.com>
2026-05-21 11:33:11 -0400

    fix deployment bug, removing base from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://papagreeen.github.io',
-    base: '/sushi-bby', // replace with your exact repo name
-
+    site: 'https://papagreeen.github.io'
 });
