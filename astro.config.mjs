b2ec0bb5e32ed9349e48d264370afcf5cd6a39f6
PapaGreen <jacob.wilder826@gmail.com>
2026-05-21 11:44:49 -0400

    updated pathing base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://papagreeen.github.io'
+    site: 'https://<your-github-username>.github.io',
+    base: '/sushi-bby',
 });
