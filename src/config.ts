c3251672262770b25f69b511b2fba62cf9c7ae5f
PapaGreeen <jacob.wilder826@gmail.com>
2026-07-24 00:54:27 -0400

    cleaned up code/ updated for wcag/ security vulnerabitilites

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,11 @@ export const siteConfig = {
     { label: "Hours & Location", href: "/hours" },
     { label: "Reservations", href: "/reservations" },
   ],
-  address: "1832 East 2nd Street, Scotch Plains, NJ 07676",
+  address: "1832 East 2nd Street, Scotch Plains, NJ 07076",
   phone: "(555) 000-0000",
   email: "contact@sushi-bby-omakase.com",
   hours: [
-    { day: "Sunday – Thursday", time: "5:30am – 8:30pm" },
+    { day: "Sunday – Thursday", time: "5:30pm – 8:30pm" },
     { day: "Friday - Saturday", time: "5:30pm – 10pm" },
   ],
 };
